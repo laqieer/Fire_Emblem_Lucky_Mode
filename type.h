d8b2fe7d4c2180a3395ac178c35b045ad719c031
laqieer <laqieer@126.com>
2018-11-06 16:25:29 +0800

    define common types

---
+++ type.h
@@ -0,0 +1,43 @@
+#pragma once
+
+typedef     unsigned char           u8;
+typedef     unsigned short int      u16;
+typedef     unsigned int            u32;
+typedef     unsigned long long int  u64;
+
+typedef     signed char             s8;
+typedef     signed short int        s16;
+typedef     signed int              s32;
+typedef     signed long long int    s64;
+
+typedef		unsigned int			uint;
+typedef		unsigned char			uchar;
+typedef		unsigned short			ushort;
+typedef		unsigned long			ulong;
+
+typedef		char   					int8;
+typedef		signed char				sint8;
+typedef		unsigned char			uint8;
+typedef		char					__int8;
+typedef		short					int16;
+typedef		signed short			sint16;
+typedef		unsigned short			uint16;
+typedef		short					__int16;
+typedef		int						int32;
+typedef		signed int				sint32;
+typedef		unsigned int			uint32;
+typedef		int						__int32;
+typedef		long long				int64;
+typedef		long long				sint64;
+typedef		unsigned long long		uint64;
+typedef		long long				__int64;
+
+typedef		int8					BYTE;
+typedef		int16					WORD;
+typedef		int32					DWORD;
+typedef		int64					QWORD;
+
+typedef		uint8					_BYTE;
+typedef		uint16					_WORD;
+typedef		uint32					_DWORD;
+typedef		uint64					_QWORD;
